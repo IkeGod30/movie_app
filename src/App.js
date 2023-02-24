74068c91c9c2c2a8ba396d3cbf4eaab95a51e07f
unknown <anthoizu@yahoo.com>
2023-02-23 20:54:49 -0500

    created css module, added html to App

--- src/App.js
+++ src/App.js
@@ -5,9 +5,22 @@ import s from './style.module.css';
 function App() {
   return (
     <div className={s.main_container}>
-      <div className={s.header}></div>
-      <div className={s.tv_show_details}></div>
-      <div className={s.recommended_tv_shows}></div>
+      <div className={s.header}>
+        <div className="row">
+          <div className="col-4">
+            <div>Logo</div>
+            <div>Subtitle</div>
+
+          </div>
+           <div className="col-md-12 col-lg-4">
+            <input style={{ width: "100%" }} type="text" />
+
+           </div>
+
+        </div>
+      </div>
+      <div className={s.tv_show_details}> TV Show Details</div>
+      <div className={s.recommended_tv_shows}>Recommended TV Shows</div>
     </div>
   )
 }
