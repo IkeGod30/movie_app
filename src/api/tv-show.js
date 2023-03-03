aad9610f915fc23eab4535f5c1a49b7c656f7e02
unknown <anthoizu@yahoo.com>
2023-03-02 21:23:28 -0500

    Updated tv-show.js

--- src/api/tv-show.js
+++ src/api/tv-show.js
@@ -11,10 +11,10 @@ import { BASE_URL, API_KEY_PARAM, BACKDROP_BASE_URL } from "../config";
 
 export class TVShowAPI {
     static async fetchPopulars() {
-    //    const response = await axios.get(`${BASE_URL}tv/popular${API_KEY_PARAM }`);
-    //    console.log(response.data.results);
-    //    return response.data.results;
-    return FAKE_POPULARS;
+       const response = await axios.get(`${BASE_URL}tv/popular${API_KEY_PARAM }`);
+       console.log(response.data.results);
+       return response.data.results;
+    // return FAKE_POPULARS;
 
     }
 
@@ -22,7 +22,7 @@ export class TVShowAPI {
            const response = await axios.get(
             `${BASE_URL}tv/${tvShowId}/recommendations${API_KEY_PARAM }`
             );
-           console.log(response.data.results);
+        //    console.log(response.data.results);
            return response.data.results;
 
         // return FAKE_RECOMMENDATIONS;
