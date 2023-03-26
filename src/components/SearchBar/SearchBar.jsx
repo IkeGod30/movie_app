05d4fbeed1b926e672e3d06241cf4ea89e7fd829
unknown <anthoizu@yahoo.com>
2023-03-26 18:41:52 -0400

    Updated searchbar component

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,11 +1,18 @@
 import s from "./style.module.css";
 import { Search as SearchIcon } from "react-bootstrap-icons";
 
-export function SearchBar(props) {
+export function SearchBar({ onSubmit }) {
+    function submit(e) {
+        if(e.key == "Enter" && e.target.value.trim() != "")
+        onSubmit(e.target.value);
+        // console.log("Send Search")
+    //    console.log(e.target.value); 
+        
+    }
     return(
         <>
         <SearchIcon size={27} className={s.icon} />
-        <input className={s.input} type="text" placeholder={"Look up a Show"} />
+        <input onKeyUp={submit} className={s.input} type="text" placeholder={"Look up a Show"} />
         </>
     );
 }
