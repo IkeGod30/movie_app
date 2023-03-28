ac5241b8621df61ed6471e903f060e2ebff9f627
unknown <anthoizu@yahoo.com>
2023-03-28 18:52:01 -0400

    Updated the search logic

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,12 +1,15 @@
 import s from "./style.module.css";
 import { Search as SearchIcon } from "react-bootstrap-icons";
 
+
 export function SearchBar({ onSubmit }) {
     function submit(e) {
-        if(e.key == "Enter" && e.target.value.trim() != "")
+        if(e.key === "Enter" && e.target.value.trim() !== "")
         onSubmit(e.target.value);
-        // console.log("Send Search")
-    //    console.log(e.target.value); 
+
+    //    if(e.target.value === "") 
+    //    alert('Please type a valid value into the search bar');
+       
         
     }
     return(
